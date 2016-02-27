a2a7ebbfb999bd76a136d71d17d9f856148ef966
dwin357 <e.r.steinmetz@gmail.com>
2016-02-27 10:54:05 -0600

    theory error was due to bad controller finish

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,11 +1,11 @@
 class ChargesController < ApplicationController
 
   def new
-    
+
   end
 
   def create
-    @amount = 500 #this is $5, min charge is $0.50
+    @amount = 1000 #this is $5, min charge is $0.50
 
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail],
@@ -23,4 +23,6 @@ class ChargesController < ApplicationController
     flash[:error] = e.message
     redirect_to new_charge_path
   end
+
+  redirect_to secret_path
 end
